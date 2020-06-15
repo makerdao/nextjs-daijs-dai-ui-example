4a212d5c2250bb5a1689b325f16d159be08ec524
Phil Bain <bain.pmcg@gmail.com>
2020-06-15 20:43:49 +0200

    add SDK plugin shell

--- maker.js
+++ maker.js
@@ -1,17 +1,13 @@
 import Maker from '@makerdao/dai';
+// import customPlugin from './plugin/index';
 
 let maker;
-
-// For testing;
-export function getMaker() {
-  //   assert(maker, 'Maker has not been instantiated');
-  return maker;
-}
 
 export async function instantiateMaker() {
   const config = {
     log: true,
     autoAuthenticate: false,
+    // plugins: [[customPlugin, { customOption: 'test' }]], // config options can be passed to your plugin like this
   };
 
   maker = await Maker.create('browser', config);
